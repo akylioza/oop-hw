cb2007db6713ee52b90c665f4e589247eb333e18
akylioza <askarovdakyl@gmail.com>
2022-10-07 21:41:06 +0300

    first

---
+++ venv/main.py
@@ -0,0 +1,33 @@
+class Hero:
+    def __init__(self, name, nickname, hp, damage):
+        self.name = name
+        self.nickname = nickname
+        self.hp = hp
+        self.damage = damage
+
+    def heal(self):
+        print('healed ' + str(self.hp + 100))
+    def greetings(self):
+        print('my name is ' + self.name)
+    def damage_up(self):
+        print('damage boosted: ' + str(self.damage * 2))
+    def brand_phrase(self):
+        print('good will win')
+
+
+
+gon = Hero(name='Gon ', nickname='wednsday ', hp=100, damage=10)
+print(gon.name, gon.nickname, gon.hp, gon.damage)
+gon.heal()
+
+frik = Hero(name='Frik ', nickname='monday ', hp=200, damage=100)
+print(frik.name,frik.nickname,frik.hp,frik.damage)
+frik.damage_up()
+
+john = Hero(name='John ', nickname='sunday ', hp=150, damage=110)
+print(john.name,john.nickname,john.hp,john.damage)
+frik.greetings()
+
+donna = Hero(name='Donna ', nickname='tuesday ', hp=130, damage=190)
+print(donna.name,donna.nickname,donna.hp,donna.damage)
+donna.brand_phrase()
