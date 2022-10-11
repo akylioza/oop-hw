6be8c975b54020914e15ed90b35df928ff995add
akylioza <114997592+akylioza@users.noreply.github.com>
2022-10-11 09:43:05 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -4,8 +4,9 @@ from homewr1 import Hero
 class air(Hero):
     dop = 'air'
 
-    def __init__(self, name, nickname, hp, damage):
+    def __init__(self, name, nickname, hp, damage, fly=False):
         super().__init__(name, nickname, hp, damage)
+        self.fly = fly
 
     def __gen_x(self):
         pass
@@ -14,8 +15,9 @@ class air(Hero):
 class earth(Hero):
     pod = 'earth'
 
-    def __init__(self, name, nickname, hp, damage):
+    def __init__(self, name, nickname, hp, damage, fly=False):
         super().__init__(name, nickname, hp, damage)
+        self.fly = fly
 
     def __gen_x(self):
         pass
@@ -24,22 +26,21 @@ class earth(Hero):
 class cosmic(Hero):
     tod = 'space'
 
-    def __init__(self, name, nickname, hp, damage):
+    def __init__(self, name, nickname, hp, damage, fly=False):
         super().__init__(name, nickname, hp, damage)
+        self.fly = fly
 
     def __gen_x(self):
         pass
 
-    def brand_phrase(self):
-        self.fly = True
-        print(f'fly in the true')
 
 
 ear = earth('semo', 'hopper', 10, 1)
-print(ear.name, ear.nickname, ear.hp, ear.damage)
+print(ear.name, ear.nickname, ear.fly)
+print(ear.brand_phrase())
 
 aer = air('erno', 'frierr', 12, 2)
-print(aer.name, aer.nickname, aer.hp, aer.damage)
+print(aer.name, aer.nickname, aer.fly)
 
 cos = cosmic('noer', 'post', 22, 4)
 print(cos.name, cos.nickname, cos.hp, cos.damage)
